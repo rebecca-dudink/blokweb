0a18341a461c4af2ae34019100cfcabed4563f19
rebecca-dudink <94301537+rebecca-dudink@users.noreply.github.com>
2021-12-03 14:11:49 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+
+var menu = document.querySelector('.menu');
+var openButton = document.querySelector('.open');
+var closeButton = document.querySelector('.close');
+
+openButton.addEventListener('click', toggleMenu);
+closeButton.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    menu.classList.toggle('menu-open');
+}
